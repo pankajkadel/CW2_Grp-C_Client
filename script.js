b4e7f83da15315671b95fccc42a9049cd49f34ee
pankajkadel <pankajkadel4@gmail.com>
2026-08-03 15:36:29 +0100

    Add the hide and see js code .

--- script.js
+++ script.js
@@ -27,3 +27,16 @@ function startSlideshow() {
 }
 startSlideshow();
 
+
+// Password hide and see
+function togglePassword(inputId, btn) {
+    const input = document.getElementById(inputId);
+    if (input.type === 'password') {
+        input.type = 'text';
+        btn.textContent = '🙈';
+    } else {
+        input.type = 'password';
+        btn.textContent = '👁️';
+    }
+}
+
